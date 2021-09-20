2403c097242b6bc84cbe57f350dfa07d82c3759a
BRIAN OTIENO <osano3204@gmail.com>
2021-09-20 10:48:38 +0300

    c-sharp program to add two numbers
    writeline,readline , consoles etc

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AddTwoNumbers
+{
+
+    class AddTwoNumbersClass
+    {
+        static void Main()
+        {
+            AddTwoNumbersClass GetTheResult = new AddTwoNumbersClass();
+        
+                var UpdatedValue = GetTheResult.AddTwoNumbersFunction();
+     
+            Console.WriteLine();
+        }
+
+        public int AddTwoNumbersFunction()
+        {
+            Console.WriteLine("Enter two numbers :");
+            int num1 = int.Parse(Console.ReadLine());
+            int numb2 =int.Parse( Console.ReadLine());
+            int sum = num1 + numb2;
+            Console.WriteLine("The sum of the two numbers is: " + sum);
+
+            return sum;
+
+        }
+    }
+
+}
